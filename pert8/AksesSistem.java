b95bff74ac27d258764c6eb01ee5cb87c17ecfe7
Zahratul Askia <04jaydenraden@gmail.com>
2025-12-07 21:33:35 +0700

    update pert 8

--- pert8/AksesSistem.java
+++ pert8/AksesSistem.java
@@ -1,25 +1,21 @@
 public interface AksesSistem {
-    
+
     // ==========================================================
     // METHOD ABSTRAK (WAJIB DIIMPLEMENTASIKAN DI CLASS ProgramerMagang)
     // ==========================================================
-    
+
     // DEKLARASI METHOD 1: login
-    // Menerima satu parameter: String pin
-    // Mengembalikan nilai: void
-    
-    
+    void login(String pin);
+
     // DEKLARASI METHOD 2: logout
-    // Tidak menerima parameter
-    // Mengembalikan nilai: void
-    
-    
+    void logout();
+
     // ==========================================================
     // DEFAULT METHOD (OPSIONAL DI-OVERRIDE)
     // ==========================================================
-    
+
     // DEKLARASI DEFAULT METHOD: getRoleAkses
-    // Tidak menerima parameter
-    // Mengembalikan nilai: String "Staff Biasa"
-    
+    default String getRoleAkses() {
+        return "Staff Biasa";
+    }
 }
---
+++ pert8/UjiSDM.java
@@ -0,0 +1,23 @@
+public class UjiSDM {
+    public static void main(String[] args) {
+        System.out.println("=== PENGUJIAN PROGRAMMER MAGANG ===\n");
+
+        // 1. BUAT OBJEK ProgrammerMagang
+        ProgrammerMagang andi = new ProgrammerMagang("Andi", 50000.0, "1234");
+
+        // 2. PENGUJIAN METODE BERURUTAN
+        andi.hitungGaji(160);
+
+        System.out.println("Status Cuti: " + andi.getStatusCuti());
+
+        andi.login("9999");
+
+        andi.login("1234");
+
+        System.out.println("Role Akses: " + andi.getRoleAkses());
+
+        andi.perpanjangKontrak(6);
+
+        andi.logout();
+    }
+}
