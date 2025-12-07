aa421301a826cc1dd766449b76e74ee4df91771f
Zahratul Askia <04jaydenraden@gmail.com>
2025-12-07 14:37:13 +0700

    nambahin file

--- pert3/Manajer.java
+++ pert3/Manajer.java
@@ -1,16 +1,20 @@
-//// Gunakan keyword untuk mewarisi class Karyawan
-//class Manajer ... {
-//    // Deklarasikan variabel tambahan khusus Manajer (tunjangan)
-//
-//
-//    // Constructor
-//    // Tips: Gunakan 'super' untuk memanggil constructor parent
-//
-//
-//    // Method Override tampilInfo
-//    // Tips: Tampilkan info dasar, lalu tambahkan info tunjangan dan total gaji
-//    @Override
-//    void tampilInfo() {
-//
-//    }
-//}
+// Gunakan keyword untuk mewarisi class Karyawan
+class Manajer extends Karyawan {
+    // Deklarasikan variabel tambahan khusus Manajer (tunjangan)
+    private double tunjangan;
+
+    // Constructor
+    // Tips: Gunakan 'super' untuk memanggil constructor parent
+    public Manajer(String nama, double gajiPokok, double tunjangan) {
+        super(nama, gajiPokok); // Memanggil contrustor parent
+        this.tunjangan = tunjangan;
+    }
+
+    // Method Override tampilInfo
+    // Tips: Tampilkan info dasar, lalu tambahkan info tunjangan dan total gaji
+    @Override
+    void tampilInfo() {
+        System.out.println("Nama: " + nama + " | Gaji Pokok: Rp " + gajiPokok + " | Tunjangan: Rp " + tunjangan);
+        System.out.println("Total Pendapatan: Rp " + (gajiPokok + tunjangan));
+    }
+}
